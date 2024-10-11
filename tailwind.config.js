a275788d90df03153231378436f2def5991afdb9
Fel_Saya-ao <146634912+SatouriKanade@users.noreply.github.com>
2024-10-11 12:41:10 +0000

    trying out tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',  // Your HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
